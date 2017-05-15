ae5959d2de7c481fec8a16ce5820b576c60646a3
Sergey Visman <admin@karbidsoft.ru>
2017-05-16 00:42:03 +0300

    Добавлены тесты, не работают пока нужно разбираться как тестировать модели

--- animan/tests.py
+++ animan/tests.py
@@ -1,3 +1,16 @@
 from django.test import TestCase
+from .models import Pet
 
 # Create your tests here.
+
+
+def create_pet(name='Белка', age=10, weight=7, height=50, comment='Первый космонавт'):
+    return Pet.objects.create(name=name, age=age, weight=weight, height=height, comment=comment)
+
+
+class PetCreationTest(TestCase):
+
+    def test_create_pet(self):
+        dog = create_pet()
+        self.assertTrue(isinstance(dog, Pet))
+        self.assertEqual(dog.name, 'Белка')
